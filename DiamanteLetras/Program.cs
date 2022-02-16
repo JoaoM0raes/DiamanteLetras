a53298152c349e39b9e3700d71fbc6676533f106
João Moraes <Joo Moraes@DESKTOP-2QU5RNS>
2022-02-16 16:32:40 -0300

    Add project files.

---
+++ DiamanteLetras/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DiamanteLetras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string letra;
+            int linhas;
+            string letras = "abcdefghijklmnopqrstuvwxyz";
+            Console.WriteLine("Escreva uma letra minúscula");
+            letra = Console.ReadLine();
+            linhas = letras.IndexOf(letra);
+            int espaçosLinhas = linhas;
+            int somaEspaços = 0;
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int k = 0; k < espaçosLinhas; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(letras[i]);
+                for (int l = 0; l < somaEspaços; l++)
+                {
+                    Console.Write(" ");
+                }
+                if (i >= 1)
+                {
+                    Console.Write(letras[i]);
+                }
+                somaEspaços += 2;
+                espaçosLinhas--;
+                Console.WriteLine();
+            }
+            for (int i = linhas; i >=0; i--)
+            {
+                for (int k = 0; k < espaçosLinhas; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write(letras[i]);
+                for (int l = 0; l < somaEspaços; l++)
+                {
+                    Console.Write(" ");
+                }
+                if (i>=1 && i <= linhas)
+                {
+                    Console.Write(letras[i]);
+                }                                        
+                somaEspaços -= 2;
+                espaçosLinhas++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
